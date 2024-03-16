90bfb98782f177c5e1889d8767cafe76b2c542d4
Ilze Rigerte <irigerte@gmail.com>
2024-03-16 15:42:41 +0200

    emial remove

--- src/main/java/com/ai/choice/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-//package com.ai.choice.service;
-//
-//
-//import com.ai.choice.enumeration.VerificationType;
-//
-//public interface EmailService {
-//
-//    void sendVerificationEmail(String firstName, String email, String verificationUrl, VerificationType verificationType);
-//}
